0412ef8e05c2eea2ae885dc5cdbfdd88f7882f23
kaosay <58497583+kaosay@users.noreply.github.com>
2025-11-13 14:11:35 +0800

    Update migration script for new repository URL

--- migration.sh
+++ migration.sh
@@ -2,8 +2,11 @@
 
 # How to migration repository to new place!
 
-git clone http://10.0.0.1:3000/IG/ser-c.git --mirror
+#git clone http://10.0.0.1:3000/IG/ser-c.git --mirror
+git clone http://kaosay@10.0.0.1:3000/IG/ser-c.git --mirror
 
+cd ser-c.git
 git remote set-url origin http://10.0.0.2:8096/IG/ser-c.git
 
 git push --mirror origin
+cd ..
